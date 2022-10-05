fd5ec62d745a48875abc3f2b77001f4ef9844492
Paul Voelker <healingwaters7@hotmail.com>
2022-10-04 22:01:10 -0500

    Adjusted layout on quiz page and made font size dynamic on the buttons

---
+++ JBQQuizMeApp/Converters/TextLengthToFontSize.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JBQQuizMeApp.Converters
+{
+    public class TextLengthToFontSize : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var strVal = (string)value;
+            var strValLen = strVal.Length;
+
+            var fontSizeConverter = new FontSizeConverter();
+
+            if (strValLen > 50)
+            {
+                return fontSizeConverter.ConvertFromString("Micro");
+
+            }
+            else if(strValLen > 25)
+            {
+                return fontSizeConverter.ConvertFromString("Small");
+
+            }
+            else
+            {
+                return fontSizeConverter.ConvertFromString("Medium");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
