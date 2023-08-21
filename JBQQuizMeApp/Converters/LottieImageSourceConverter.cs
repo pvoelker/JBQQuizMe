8cb65c80152aff100a902d3bad9552a78ffdff57
Paul Voelker <healingwaters7@hotmail.com>
2023-08-20 22:19:34 -0500

    Get SkiaSharp.Extended.UI.Maiu implementation out of view model

---
+++ JBQQuizMeApp/Converters/LottieImageSourceConverter.cs
@@ -0,0 +1,31 @@
+﻿using SkiaSharp.Extended.UI.Controls;
+using SkiaSharp.Extended.UI.Controls.Converters;
+using System;
+using System.Reflection;
+
+namespace JBQQuizMeApp.Converters
+{
+    public class LottieImageSourceConverter : IValueConverter
+    {
+        private static SKLottieImageSourceConverter _lottieConverter = new SKLottieImageSourceConverter();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var strVal = (string)value;
+
+            if (strVal == null)
+            {
+                return null;
+            }
+            else
+            {
+                return (SKLottieImageSource)_lottieConverter.ConvertFromString(strVal);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
